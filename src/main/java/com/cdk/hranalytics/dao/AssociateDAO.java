b663c1d1058bf08737d2f3516799cd8d0420d508
dullus <Qwerty@12345>
2016-08-30 19:07:52 +0530

    created classes in dao

--- src/main/java/com/cdk/hranalytics/dao/AssociateDAO.java
+++ src/main/java/com/cdk/hranalytics/dao/AssociateDAO.java
@@ -1,5 +1,6 @@
 package com.cdk.hranalytics.dao;
 
+import com.cdk.hranalytics.domain.Associate;
 import org.springframework.orm.hibernate4.HibernateTemplate;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +16,20 @@ public class AssociateDAO {
     public void setHibernateTemplate(HibernateTemplate hibernateTemplate) {
         this.hibernateTemplate = hibernateTemplate;
     }
+
+    public Associate save(Associate associate){
+        Associate associate1 = new Associate();
+        associate1.setId(associate.getId());
+        associate1.setFirstName(associate.getFirstName());
+        associate1.setLastName(associate.getLastName());
+        associate1.setLocation(associate.getLocation());
+        associate1.setDateOfJoining(associate.getDateOfJoining());
+        associate1.setReportingManager(associate.getReportingManager());
+        associate1.setLastPromotionDate(associate.getLastPromotionDate());
+        hibernateTemplate.save(associate1);
+        return associate;
+    }
+
 
 
 
